db737ee9670387dd623f2d2025d269ddaa4d60b9
홍혁준 <jjj8337721@naver.com>
2022-08-26 16:59:10 +0900

    feat : spring batch 기본 설정 및 테스팅.

---
+++ src/main/java/kr/co/springbatchstudy/JobConfig.java
@@ -0,0 +1,51 @@
+package kr.co.springbatchstudy;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class JobConfig {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job testJob(){
+        return jobBuilderFactory.get("Test Job")
+                .start(testStep1())
+                .next(testStep2())
+                .build();
+    }
+
+    @Bean
+    public Step testStep1() {
+        return stepBuilderFactory.get("Test Step1")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("=================");
+                    System.out.println("Test Spring Batch");
+                    System.out.println("=================");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Step testStep2() {
+        return stepBuilderFactory.get("Test Step2")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("=================");
+                    System.out.println("Test Step2 Start");
+                    System.out.println("=================");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+}
