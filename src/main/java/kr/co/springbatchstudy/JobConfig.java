121df4b80206bc4f7217774a56eb38fb4b163ada
홍혁준 <jjj8337721@naver.com>
2022-09-04 13:45:26 +0900

    feat : JobParameter 테스트

--- src/main/java/kr/co/springbatchstudy/JobConfig.java
+++ src/main/java/kr/co/springbatchstudy/JobConfig.java
@@ -2,12 +2,15 @@ package kr.co.springbatchstudy;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
 
 @Configuration
 @RequiredArgsConstructor
@@ -28,6 +31,17 @@ public class JobConfig {
     public Step testStep1() {
         return stepBuilderFactory.get("Test Step1")
                 .tasklet((contribution, chunkContext) -> {
+
+                    //이 방식을 주로 사용.
+                    JobParameters jobParameters = contribution.getStepExecution().getJobExecution().getJobParameters();
+                    jobParameters.getString("name");
+                    jobParameters.getLong("seq");
+                    jobParameters.getDate("date");
+                    jobParameters.getDouble("price");
+
+                    //동일한 값을 얻을 수는 있지만 Map으로 받는다. (즉, 값으로만 얻을 수 있음)
+                    Map<String, Object> mapJobParameters = chunkContext.getStepContext().getJobParameters();
+
                     System.out.println("=================");
                     System.out.println("Test Step1 Start");
                     System.out.println("=================");
